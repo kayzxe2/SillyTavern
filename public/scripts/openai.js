c573e55237f6c79474a9413ec5385479123ec85d
maver <kentucky@posteo.de>
2023-06-03 21:49:18 +0200

    Fix default prompts not being set correctly

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -1015,9 +1015,9 @@ function loadOpenAISettings(data, settings) {
     oai_settings.use_openrouter = settings.use_openrouter ?? default_settings.use_openrouter;
     oai_settings.api_url_scale = settings.api_url_scale ?? default_settings.api_url_scale;
 
-    oai_settings.prompts = settings.prompts ?? []
-    oai_settings.prompt_lists = settings.prompt_lists ?? [];
-    oai_settings.prompt_manager_settings = settings.prompt_manager_settings ?? [];
+    oai_settings.prompts = settings.prompts ?? default_settings.prompts;
+    oai_settings.prompt_lists = settings.prompt_lists ?? default_settings.prompt_lists;
+    oai_settings.prompt_manager_settings = settings.prompt_manager_settings ?? default_settings.prompt_manager_settings;
 
     if (settings.nsfw_toggle !== undefined) oai_settings.nsfw_toggle = !!settings.nsfw_toggle;
     if (settings.keep_example_dialogue !== undefined) oai_settings.keep_example_dialogue = !!settings.keep_example_dialogue;

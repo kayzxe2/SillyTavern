6844fc10c0fc2c6e398b9ce50c6442b2c5d3c502
maver <kentucky@posteo.de>
2023-06-15 18:11:03 +0200

    Fix chat completion not recognizing 0-position

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -1004,7 +1004,7 @@ class ChatCompletion {
         this.validateMessageCollection(collection);
         this.checkTokenBudget(collection, collection.identifier);
 
-        if (position) {
+        if (null !== position && -1 !== position) {
             this.messages.collection[position] = collection;
         } else {
             this.messages.collection.push(collection);
